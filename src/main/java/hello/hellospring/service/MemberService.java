7a13137ced4caf1991188e67dc678b8d0bf59f4f
pshyeok <pshyeok@kakao.com>
2023-09-11 19:26:50 +0900

    memberservice test

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,11 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     /**회원가입**/
     public Long join(Member member){
@@ -24,7 +28,6 @@ public class MemberService {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
-
     /** 전체회원 조회**/
     public List<Member> findMembers() {
         return memberRepository.findAll();
