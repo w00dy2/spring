f710b9face1ed737aa1d4729ce5f9bc3c5b6631f
pshyeok <pshyeok@kakao.com>
2023-09-18 11:38:19 +0900

    starting spring end

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -42,14 +42,7 @@ public class MemberService {
     }
     /** 전체회원 조회**/
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try {
             return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers" + timeMs + "ms");
-        }
     }
 
     public Optional<Member> findOne(Long memberId) {
