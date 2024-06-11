5a50c32d6fd496c709587c990773fa1ad3babc37
hyeok <pshyeok@kakao.com>
2024-06-11 09:44:59 +0900

    merge basic & start board

---
+++ hello/src/hello/HelloWorld.java
@@ -0,0 +1,21 @@
+package hello;
+//이클립스 = ctrl + f11
+//인텔리제이 = shift + f10
+public class HelloWorld {
+	// 스프링 부트 연습을 위한 첫번째 테스트
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("헬로우월드");
+		System.out.println("헬로우월드");
+		System.out.println("헬로우월드");
+		System.out.println("헬로우월드");
+		System.out.println("헬로우월드");
+		System.out.println("헬로우월드");
+		System.out.println("헬로우월드");
+		System.out.println("헬로우월드");
+		System.out.println("헬로우월드");
+		System.out.println("헬로우월드");
+		
+	}
+
+}
